805b9800113e1fa0d454dd7d59264ee5d90677bf
YoonSung Kim <opqudk@gmail.com>
2026-01-01 15:57:48 +0900

    :tada: chore: vite init (#49)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Crypto Market Insight</h1>
+      <p>Frontend is ready!</p>
+    </div>
+  )
+}
+
+export default App
