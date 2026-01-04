358c09b5ecef42297e8bd34db8b382b6b5f56b4b
YoonSung Kim <opqudk@gmail.com>
2026-01-04 09:57:11 +0900

    ✨ feat: add request interceptor (#56)

--- frontend/src/features/common/api/client.ts
+++ frontend/src/features/common/api/client.ts
@@ -7,3 +7,11 @@ export const client = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+client.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
